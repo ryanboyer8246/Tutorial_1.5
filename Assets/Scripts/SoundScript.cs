bbd70eca8d674aaf5b0ce06ca9e53ac1e9f77190
Ryan Boyer <ryanboyer8246@knights.ucf.edu>
2021-09-29 00:51:01 -0400

    Second Commit

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SoundScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioClip musicClipOne;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public AudioClip musicClipTwo;
+
+public AudioSource musicSource;
+
+// Update is called once per frame
+void Update()
+{
+     if (Input.GetKeyDown(KeyCode.W))
+        {
+          musicSource.clip = musicClipOne;
+          musicSource.Play();
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+        {
+          musicSource.Stop();
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.R))
+        {
+          musicSource.clip = musicClipTwo;
+          musicSource.Play();
+         }
+
+     if (Input.GetKeyUp(KeyCode.R))
+        {
+          musicSource.Stop();
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.L))
+        {
+          musicSource.loop = true;
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+        {
+          musicSource.loop = false;
+        }
+   }
 }
